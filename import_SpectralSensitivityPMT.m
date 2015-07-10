a37337191ee89b9249c3eca17ee34deb9f684c60
petteriTeikari <petteri.teikari@gmail.com>
2015-07-10 16:15:07 -0400

    PMT sensitivity
    PMT Sensitivities added from:
    http://www.olympusmicro.com/primer/techniques/confocal/detectorsintro.html

--- import_SpectralSensitivityPMT.m
+++ import_SpectralSensitivityPMT.m
@@ -7,14 +7,16 @@ function PMTs = import_SpectralSensitivityPMT(wavelength)
     
     % PMT Ga-As-P (UV-Visible Range)
     ind = 1;
-    PMTs{ind}.wavelength = wavelength;
-    PMTs{ind}.sensitivity = ones(length(wavelength), 1);
+    tmpData = importdata(fullfile('data','pmt_Sensitivities_x300-680nm_Ga-As.txt'), ',', 1)
+    PMTs{ind}.wavelength = tmpData.data(:,1);
+    PMTs{ind}.sensitivity = tmpData.data(:,2);
     PMTs{ind}.name = 'PMT Ga-As-P';
     PMTs{ind}.plotColor = [0.4 0.4 0];
     
     % PMT Ga-As (UV-Visible-NIR Range)
     ind = ind+1;
-    PMTs{ind}.wavelength = wavelength;
-    PMTs{ind}.sensitivity = ones(length(wavelength), 1);
+    tmpData = importdata(fullfile('data','pmt_Sensitivities_x300_840nm_Ga-As-P.txt'), ',', 1);
+    PMTs{ind}.wavelength = tmpData.data(:,1);
+    PMTs{ind}.sensitivity = tmpData.data(:,2);
     PMTs{ind}.name = 'PMT Ga-As';
     PMTs{ind}.plotColor = [0 0 0];
