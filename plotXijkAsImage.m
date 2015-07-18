9af7e6773652d6167b8fb4e51c309c2b41cd210a
petteriTeikari <petteri.teikari@gmail.com>
2015-07-18 18:27:03 -0400

    Xijk matrix with text

--- plotXijkAsImage.m
+++ plotXijkAsImage.m
@@ -3,29 +3,55 @@ function plotXijkAsImage(fig, scrsz, Xijk, upscaleFactor, fluoroEmission, channe
     % now we for example 3x4 matrix which will be so tiny when plotted
     XijkImage = imresize(Xijk.matrix, upscaleFactor, 'nearest');
     imshow(XijkImage)
-    colormap('jet')
+    colormap('pink')
+        % TODO: Custom colormap, that visualizes the differences like jet
+        % but allows text to be overlaid with decent readability
     colorbar
 
     % now the excitation scalar matrix 
+    absXijk = squeeze(Xijk.matrixAbs);
     kcps = squeeze(Xijk.excitationScalar); % fix for multiple light sources
     
     xTickLocs = upscaleFactor * (0.5:1:(size(Xijk.matrix,2) - 0.5));
     yTickLocs = upscaleFactor * (0.5:1:(size(Xijk.matrix,1) - 0.5));
     axis on
-
+    whos
+    
+    % for text
+    yOffset = (yTickLocs(2) - yTickLocs(1)) * 0.15;
+    thrTextColor = 0.5;
+    
+    
+    % get trace of the kcps
     for i = 1 : size(kcps,1)
         for j = 1 : size(kcps, 2)
             if i == j
-                diagonalEmissions(j) = kcps(i,j);
+                diagonalAbsEmissions(j) = kcps(i,j);
+                absXijkDiag(j) = absXijk(i,j);
             end
+            t(i,j,1) = text(xTickLocs(j), yTickLocs(i)-yOffset, num2str(Xijk.matrix(i,j),2));
+            t(i,j,2) = text(xTickLocs(j), yTickLocs(i)+yOffset, num2str(absXijk(i,j),2));
+            % t(i,j,3) = text(xTickLocs(j), yTickLocs(i)+(2*yOffset), num2str(kcps(i,j),2));
+            
+            % fix the contrast issue
+                if Xijk.matrix(i,j) > thrTextColor
+                    set(t(i,j,:), 'Color', [0 0 0])
+                else
+                    set(t(i,j,:), 'Color', [1 1 1])
+                end          
+            
         end
     end
     
-    diagonalEmissions
+    % diagonalEmissions
     
     set(gca, 'YTickLabel', fluoroEmission.name, 'YTick', yTickLocs)
     set(gca, 'XTickLabel', channelMatrix.name, 'XTick', xTickLocs)
-
+    set(gca, 'FontSize', 8)
+    
+    set(t, 'HorizontalAlignment', 'center', 'FontSize', 6)
+    set(t(:,:,1), 'FontWeight', 'bold', 'FontSize', 7)
+    
     title('X_i_j_k', 'FontWeight', 'bold', 'FontSize', 11)
     
-    export_fig(fullfile('figuresOut', 'Xijk_ImageMatrix_630nmDichroic.png'), '-r200', '-a1')
+    export_fig(fullfile('figuresOut', 'Xijk_ImageMatrix_.png'), '-r200', '-a1')
